ad5e4afddea542d92be37ee17543c24708b115c1
Mateus Eduardo <45969051+mateusedua@users.noreply.github.com>
2024-08-16 21:48:24 -0300

    chore: add component of search

---
+++ src/app/_components/search.tsx
@@ -0,0 +1,52 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { Form, FormControl, FormField, FormItem, FormMessage } from "./ui/form"
+import { Input } from "./ui/input"
+import { Button } from "./ui/button"
+import { SearchIcon } from "lucide-react"
+
+const formSchema = z.object({
+  search: z.string().min(1, {
+    message: "Digite algo para Buscar.",
+  }),
+})
+
+const Search = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      search: "",
+    },
+  })
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex gap-2">
+        <FormField
+          control={form.control}
+          name="search"
+          render={({ field }) => (
+            <FormItem className="w-full">
+              <FormControl>
+                <Input placeholder="Buscar" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button>
+          <SearchIcon />
+        </Button>
+      </form>
+    </Form>
+  )
+}
+
+export default Search
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
-import Image from "next/image"
+import Search from "./_components/search"
 
 export default function Home() {
-  return <div>Home</div>
+  return (
+    <div>
+      <div className="p-6">
+        <h2 className="text-xl font-bold">Olá, Mateus!</h2>
+        <p>Sexta, 2 de Fevereiro</p>
+      </div>
+      <div className="p-6">
+        <Search />
+      </div>
+    </div>
+  )
 }
