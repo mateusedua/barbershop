ed555cc8883c990200816a06b90d68b900dac4e9
Mateus Eduardo <45969051+mateusedua@users.noreply.github.com>
2024-08-20 00:17:44 -0300

    chore: add file for add interfaces and types

---
+++ src/app/_constants/interfaces.tsx
@@ -0,0 +1,13 @@
+export type BarberByIDProps = {
+  barber_name: string
+  city: string
+  numero: string
+  complement: string
+  barber_description: string
+  barber_image_url: string
+  service_name: string
+  service_description: string
+  service_image_url: string
+  price: number
+  id_service: string
+}
