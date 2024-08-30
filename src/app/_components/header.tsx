3c64b17b2ae6e5502dc12a5bf8a82f508ad2a004
Mateus Eduardo <45969051+mateusedua@users.noreply.github.com>
2024-08-29 21:01:31 -0300

    chore: add link logo

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -6,12 +6,15 @@ import { Menu } from "lucide-react"
 import { Button } from "./ui/button"
 import { Sheet, SheetTrigger } from "./ui/sheet"
 import SidebarSheet from "./sidebar-sheet"
+import Link from "next/link"
 
 const Header = () => {
   return (
     <Card className="rounded-none">
-      <CardContent className="flex justify-between p-6">
-        <Image alt="logo" src="/Logo.svg" width={120} height={18} />
+      <CardContent className="flex items-center justify-between p-6">
+        <Link href="/">
+          <Image alt="logo" src="/Logo.svg" width={120} height={18} />
+        </Link>
         <Sheet>
           <SheetTrigger asChild>
             <Button size="icon" variant="outline">
